8b5df6f27bbae221fadb53fe40980072d56aa273
caiooliveira01 <122302892+caiooliveira01@users.noreply.github.com>
2023-05-08 12:38:18 -0300

    Projeto 1

--- primeiro-react/src/index.js
+++ primeiro-react/src/index.js
@@ -1,19 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import './style.css'
 
-function Page() {
+function App() {
   return (
-      <div>
-          <Header />
-          <MainContent />
-          <Footer />
-      </div>
+    <div className='container'>
+      <Navbar />
+      <Main />
+    </div>
   )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Page />);
+root.render(<App />);
